b7b6b58936e5a09663ccb5001f1b4660cad7535a
kobe <kobe@habitfactory.co>
2025-05-14 16:19:16 +0900

    chore: inject runtime env

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
+    <h1>런타임 환경변수 테스트</h1>
+    <h1>{{ $config.public.apiUrl }}</h1>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+  const { $config } = useNuxtApp()
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.API_URL
+    }
+  }
 })
