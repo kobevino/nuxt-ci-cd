35a9a3f54d4388e00fe0f403e290a60f44a30f83
kobe <kobe@habitfactory.co>
2026-01-27 17:34:42 +0900

    chore: a href 변경

--- pages/main.vue
+++ pages/main.vue
@@ -4,7 +4,8 @@
       <h1>Main Page</h1>
       <nav class="nav">
         <NuxtLink to="/main" class="nav-link active">Main</NuxtLink>
-        <NuxtLink to="/test" class="nav-link">Test</NuxtLink>
+        <!-- <NuxtLink to="/test" class="nav-link">Test</NuxtLink> -->
+        <a href="/test">Test alb 경로 분산</a>
       </nav>
     </header>
 
